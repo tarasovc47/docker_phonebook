50f65708e2f8a4bb5f55dba07d807f8bf0b71d06
tarasovc47 <tarasovc47@live.ru>
2021-09-12 13:11:29 +0500

    Код почищен от "тестовых" страничек

--- www/phonebook/index.php
+++ www/phonebook/index.php
@@ -1,2 +1,26 @@
 <?php
-echo "test";
+# единая точка входа
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); # избавляемся от аргументов, типа localhost/test?id=100500
+$segments = explode('/', trim($uri, '/')); # разбиваем путь на сегменты
+
+if ($segments[0] === 'auth') # если первый сегмент auth
+{
+    if ($segments[1] === 'signup') # а второй - регистрация
+    {
+        $file = 'auth/signup.php'; # подключаем страницу регистрации
+    }
+    elseif ($segments[1] === 'signin') # а второй - авторизация
+    {
+        $file = 'auth/signin.php'; # подключаем страничку авторизации
+    }
+    else $file = '404.php'; # иначе 404
+}
+
+else # если без /admin/
+{
+    if ($uri === '/') # то тут тоже простенький роутер
+        $file = 'main.php';
+    else $file = '404.php';
+}
+
+require 'views/' . $file; # подключаем полученный файл
