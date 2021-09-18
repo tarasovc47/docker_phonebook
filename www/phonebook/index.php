31e2d0e933e9ab6c2fcbc9b6a3eb0d1d3af264b4
tarasovc47 <tarasovc47@live.ru>
2021-09-18 12:22:50 +0500

    Вынес роутер в отдельный класс

--- www/phonebook/index.php
+++ www/phonebook/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use system\core\Autoloader;
+use system\core\Router;
 
 try {
     ini_set('display_errors', 0);
@@ -9,32 +10,7 @@ try {
     define('ROOT', dirname(__FILE__) . DIRECTORY_SEPARATOR);
     require_once (ROOT . 'system/core/Autoloader.php');
     Autoloader::register();
+    Router::start();
 } catch (Exception $e) {
     var_dump($e);
 }
-
-/*# единая точка входа
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); # избавляемся от аргументов, типа localhost/test?id=100500
-$segments = explode('/', trim($uri, '/')); # разбиваем путь на сегменты
-
-if ($segments[0] === 'auth') # если первый сегмент auth
-{
-    if ($segments[1] === 'signup') # а второй - регистрация
-    {
-        $file = 'auth/signup.php'; # подключаем страницу регистрации
-    }
-    elseif ($segments[1] === 'signin') # а второй - авторизация
-    {
-        $file = 'auth/signin.php'; # подключаем страничку авторизации
-    }
-    else $file = '404.php'; # иначе 404
-}
-
-else # если без /admin/
-{
-    if ($uri === '/') # то тут тоже простенький роутер
-        $file = 'main.php';
-    else $file = '404.php';
-}
-
-require 'views/' . $file; # подключаем полученный файл*/
