07a804101a5176bd939599d0ccb87cb28a20f112
tarasovc47 <tarasovc47@live.ru>
2021-10-03 17:04:24 +0500

    убрал часть тестовых конфигов

--- www/phonebook/config/config.php
+++ www/phonebook/config/config.php
@@ -5,9 +5,5 @@ return [
         'user' => \models\User::class,
         'test' => \models\Test::class,
         'test2' => \models\Test2::class,
-        'test3' => [
-            'test4' => \models\Test2::class,
-            'test5' => \models\Test2::class,
-        ],
     ]
 ];
