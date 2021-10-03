c90358810a37b82d7f33034dffc8264014b15dda
tarasovc47 <tarasovc47@live.ru>
2021-10-03 12:05:19 +0500

    пересмотрел и по другому понял фунционал трейта

--- www/phonebook/models/traits/AccessAction.php
+++ www/phonebook/models/traits/AccessAction.php
@@ -8,25 +8,26 @@ trait AccessAction
 {
     public function checkAction($view) # функция проверки экшена, принимает аргументом вид
     {
-        if (App::$components->session->isGuest() && !empty($this->authAction)) # если гостевая сессия но он при этом пытается залогиниться
+        if (App::$components->session->isGuest() && !empty($this->authAction)) # если гостевая сессия, и есть разрешённые гостю экшены
         {
-            foreach ($this->authAction as $action) # для такого действия
+            foreach ($this->authAction as $action) # для каждого такого действия
             {
                 $this->check($view, $action); # отправляем на проверку соответствия вида и экшена
             }
         }
-        elseif (!empty($this->guestAction)) # если же сессия просто гостевая
+        elseif (!empty($this->guestAction)) # если есть разрешённые гостю экшены
         {
             foreach ($this->guestAction as $action) {
-                $this->check($view, $action); # то отправляем на проверку (не совсем понял)
+
+                $this->check($view, $action); # то отправляем на проверку (не совсем понял, чем же они друг от друга отличаются)
             }
         }
     }
     public function check($view, $action)
     {
-        if ($view == $action) # если экшен совпадает с видом
+        if ($view == $action) # запрошенный экшен совпадает с видом
         {
-            $referer = App::$components->request-referer; # вот тут вообще мало что понял
+            $referer = App::$components->request->referer; # вот тут вообще мало что понял
             return header("Location: {$referer}");
         }
     }
